56c2833dd3d86bcadc611b5ecf4e7dd10fc42f9b
FunnyDogLol <132663211+FunnyDogLol@users.noreply.github.com>
2023-06-13 09:53:19 +0100

    Update upload_server.py

--- upload_server.py
+++ upload_server.py
@@ -2,21 +2,15 @@ from http.server import HTTPServer, SimpleHTTPRequestHandler
 
 class UploadHandler(SimpleHTTPRequestHandler):
     def do_POST(self):
-        # Specify the directory where uploaded files will be saved
         upload_dir = 'uploads'
         
-        # Retrieve the file data from the request
         file_data = self.rfile.read(int(self.headers['Content-Length']))
-        
-        # Extract the filename from the request headers
         print(self.headers)
         file_name = self.headers['filename']
         
-        # Save the uploaded file
         with open(f"{upload_dir}/{file_name}", 'wb') as f:
             f.write(file_data)
         
-        # Send a response indicating success
         self.send_response(200)
         self.end_headers()
         self.wfile.write(b'File uploaded successfully')
